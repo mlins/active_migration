1ade96a14e3cfd9b59926558b537dcf8f29cd612
Matt Lins <mattlins@gmail.com>
2008-07-25 13:21:20 -0500

    callbacks spec refactored

--- spec/callbacks_spec.rb
+++ spec/callbacks_spec.rb
@@ -1,48 +1,46 @@
-# require File.dirname(__FILE__) + '/spec_helper.rb'
-#
-# #require 'rubygems'; require 'ruby-debug'
-#
-# describe "A migration" do
-#
-#   before do
-#     @migration = ProductMigration.new
-#     @legacy_model = mock("ar_object1", :null_object => true)
-#     @active_model = mock("ar_object2", :null_object => true)
-#     @legacy_model.stub!(:name).and_return("Beer")
-#     @active_model.stub!(:name=).and_return("Beer")
-#     Product.stub!(:new).and_return(@active_model)
-#     Legacy::Product.stub!(:count).and_return(1)
-#     Legacy::Product.stub!(:find).and_return([@legacy_model])
-#   end
-#
-#   it "should call #before_run" do
-#     @migration.should_receive(:before_run)
-#     @migration.run
-#   end
-#
-#   it "should call #before_migrate_record" do
-#     @migration.should_receive(:before_migrate_record)
-#     @migration.run
-#   end
-#
-#   it "should call #before_migrate_field" do
-#     @migration.should_receive(:before_migrate_field)
-#     @migration.run
-#   end
-#
-#   it "should call #after_migrate_field" do
-#     @migration.should_receive(:after_migrate_field)
-#     @migration.run
-#   end
-#
-#   it "should call #after_migrate_record" do
-#     @migration.should_receive(:after_migrate_record)
-#     @migration.run
-#   end
-#
-#   it "should call #after_run" do
-#     @migration.should_receive(:after_run)
-#     @migration.run
-#   end
-#
-# end
+require File.dirname(__FILE__) + '/spec_helper.rb'
+
+require File.dirname(__FILE__) + '/fixtures/product_one_migration'
+
+describe "A migration" do
+
+  before do
+    @legacy_record = mock('legacy_model', :id => 1, :name => 'Beer')
+    @active_record = mock('active_model', :name= => 'Beer', :save => true)
+    Product.stub!(:new).and_return(@active_record)
+    Legacy::Product.stub!(:count).and_return(1)
+    Legacy::Product.stub!(:find).and_return([@legacy_record])
+    @migration = ProductOneMigration.new
+  end
+
+  it "should call #before_run" do
+    @migration.should_receive(:before_run).once
+    @migration.run
+  end
+
+  it "should call #before_migrate_record" do
+    @migration.should_receive(:before_migrate_record).once
+    @migration.run
+  end
+
+  it "should call #before_migrate_field" do
+    @migration.should_receive(:before_migrate_field).once
+    @migration.run
+  end
+
+  it "should call #after_migrate_field" do
+    @migration.should_receive(:after_migrate_field).once
+    @migration.run
+  end
+
+  it "should call #after_migrate_record" do
+    @migration.should_receive(:after_migrate_record).once
+    @migration.run
+  end
+
+  it "should call #after_run" do
+    @migration.should_receive(:after_run).once
+    @migration.run
+  end
+
+end
